8b2556054a3e23e54b2cecfef9a1bbd616b908de
Vinicius Farias <fsvinicius9@gmail.com>
2023-03-17 15:57:24 -0300

    namesUpper and slice

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -535,13 +535,13 @@ console.log(passengerCorrect)
 
 //Comparing emails
 
-const email =  'hello@jonas.io';
+const email = 'hello@jonas.io';
 const loginEmail = '   Hello@Jonas.Io  \n'
 
 const lowerEmail = loginEmail.toLowerCase()
 const trimmedEmail = lowerEmail.trim()
 console.log(trimmedEmail)
-const normalizedEmail  = loginEmail.toLowerCase()
+const normalizedEmail = loginEmail.toLowerCase()
 console.log(normalizedEmail)
 console.log(email === normalizedEmail)
 
@@ -549,18 +549,51 @@ console.log(email === normalizedEmail)
 //replacing
 const priceGB = '288,97E'
 
-const priceUS = priceGB.replace('E','$')
+const priceUS = priceGB.replace('E', '$')
 
 const announcement = "All passengers come to boarding door 23. Boarding door 23"
 
-console.log(announcement.replace("door"),'gate')
-console.log(announcement.replaceAll(/door/g,"gate"))
+console.log(announcement.replace("door"), 'gate')
+console.log(announcement.replaceAll(/door/g, "gate"))
 
 const plane = 'A32neo'
 console.log(plane.includes('A320'))
 console.log(plane.includes('Boeing'))
 console.log(plane.includes('Air'))
 
-if(plane.startsWith("Airbus") && plane.endsWith(neo)){
+if (plane.startsWith("Airbus") && plane.endsWith(neo)) {
   console.log("Part of the NEW Airbus family")
 }
+
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife') || baggage.includes('gun')) {
+    console.log("You are not allowed on board")
+  } else {
+    console.log("Welcome aboard!")
+  }
+}
+
+checkBaggage("I have a laptop, some foof and a pocket knife")
+checkBaggage('Socks and camera')
+checkBaggage('Got some snacks and a gun for protection')
+
+//Split string in multipal parts
+
+console.log('a+very+nice+string'.split('+'))
+console.log("Jonas Schmedtmann".split(" "))
+
+const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ')
+['Mr.', firstName, lastName.toUpperCase()].join(' ')
+consolew.log(newName)
+
+const capitalizeName = function (name) {
+  const names = name.split(" ")
+  const namesUpper = []
+  for (const n of names) {
+    namesUpper.push(n[0].toUpperCase() + n.slice())
+  }
+}
+
+capitalizeName('jessica ann smith davis')
+capitalizeName('jonas schmedtamnn')
