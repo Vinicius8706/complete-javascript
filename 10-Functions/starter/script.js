b0aec16a0381a85c6785f6409dac6b1f93a0e11c
Vinicius Farias <fsvinicius9@gmail.com>
2023-05-20 15:29:56 -0300

    changing the number of the flight

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,7 +1,7 @@
 'use strict';
 const booking = [];
 
-const createBooking = function (flightNum, numPassengeres = 1, price = 199) {
+const createBooking = function (flightNum, numPassengeres = 1, price = 199 * numPassengeres) {
   // numPassengeres = numPassengeres || 1;
   // price = price || 199;
   const booking = {
@@ -13,3 +13,24 @@ const createBooking = function (flightNum, numPassengeres = 1, price = 199) {
 }
 
 createBooking('LH123', 2, 800);
+createBooking('LH123', 2);
+createBooking('LH123', 5);
+createBooking('LH123');
+//When we dont set parameter on function, it will be undefined
+
+// How passing arguments works: value vs reference
+
+const flight = 'LH234';
+const vinicius = { name: 'Vinicius Farias Silva', passport: 2123479284 };
+const checkIn = function(flighNum,passenger) {
+  flighNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if(passenger.passport === 2123479284) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+}
+
+checkIn(flight, vinicius);
