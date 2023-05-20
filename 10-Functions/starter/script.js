f677d029e6b95dd6027139cec4c48973348731d8
Vinicius Farias <fsvinicius9@gmail.com>
2023-05-20 15:22:33 -0300

    Set default parameters inside function

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,15 @@
 'use strict';
+const booking = [];
+
+const createBooking = function (flightNum,numPassengeres,price){
+  numPassengeres = numPassengeres || 1;
+  price = price || 199;
+  const booking = {
+    flightNum,
+    numPassengeres,
+    price
+  }
+  console.log(booking);
+}
+
+createBooking('LH123');
