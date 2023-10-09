ea7e968935405f3855e1f1b1a8cb1223d6b04b5c
Vinicius Farias <fsvinicius9@gmail.com>
2023-10-09 09:45:17 -0300

    Metodo call

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const lufhansa = {
+    airline: "Lufhansa",
+    iataCode: "LM",
+    bookings: [],
+    book(flightNum,name){
+        console.log(`${name} bookead a set on ${this.airline}
+        flight ${this.iataCode}${flightNum}`)
+        this.bookings.push({flight:`${this.iataCode}${flightNum}`,name})
+    }
+}
+
+lufhansa.book(239, 'Jonas Schmedtmann')
+lufhansa.book(635,'John Smith')
+
+const eurowings ={
+    name: 'Eurowings',
+    iataCode: "EW",
+    bookings:[]
+}
+
+const book = lufhansa.book;
+
+book.call(eurowings,23,'Sara Williams')
+console.log(eurowings)
